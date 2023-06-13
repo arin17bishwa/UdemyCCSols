5def3a870c0423a7813e9f5e3b00f94f8dec5d43
bishwajit <arin17bishwa@gmail.com>
2023-06-13 20:07:17 +0530

    AC

---
+++ src/FactorPrinter.java
@@ -0,0 +1,13 @@
+public class FactorPrinter {
+    public static void printFactors(int number) {
+        if (number < 1) {
+            System.out.println("Invalid Value");
+            return;
+        }
+        for (int factor = 1; factor <= number; factor++) {
+            if (number % factor == 0) {
+                System.out.println(factor);
+            }
+        }
+    }
+}
