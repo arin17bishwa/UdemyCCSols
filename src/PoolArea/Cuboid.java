644c986efad88afc2762505bfd4b37065db4a997
bishwajit <arin17bishwa@gmail.com>
2023-06-19 19:27:20 +0530

    AC

---
+++ src/PoolArea/Cuboid.java
@@ -0,0 +1,20 @@
+package PoolArea;
+
+public class Cuboid extends Rectangle {
+
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = Math.max(0, height);
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return height * getArea();
+    }
+    
+}
