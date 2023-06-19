75a6e322903daab790f609c413cef73e25e57e82
bishwajit <arin17bishwa@gmail.com>
2023-06-19 18:00:37 +0530

    AC

---
+++ src/Cylinder/Circle.java
@@ -0,0 +1,18 @@
+package Cylinder;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = Math.max(0, radius);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    
+}
